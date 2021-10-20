a872e6a85834c0acee19e9234e7db62ef675dd6a
German Voronin <germanvoronin@Germans-MacBook-Pro.local>
2021-10-20 15:35:10 +0100

    Fetch data from json server

--- src/lib/collection.ts
+++ src/lib/collection.ts
@@ -9,18 +9,10 @@ export interface ICard {
   player: IPlayer;
 }
 
-export function fetchCollection() {
-  /**
-   * Step 2: Instead of directly returning the collection, fetch it from http://localhost:8001/cards
-   */
-  return [
-    {
-      id: 1,
-      player: {
-        firstname: 'John',
-        lastname: 'Doe',
-        birthday: '1993-07-22T08:38:50.090Z',
-      },
-    },
-  ] as ICard[];
+export async function fetchCollection() {
+  const res = await fetch('http://localhost:8001/cards', {
+    method: 'get',
+  });
+
+  return await res.json() as ICard[];
 }
--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import RenderCard from '../components/render-card/RenderCard';
 
-import { fetchCollection } from '../lib/collection';
+import { fetchCollection, ICard } from '../lib/collection';
 
 import './Collection.css';
 
 export const Collection = () => {
-  const collection = fetchCollection();
-  const card = collection[0];
+  const [collection, setCollection] = useState<null | ICard[]>(null);
 
-  /**
-   * Step 1: Render the card
-   */
-  return <RenderCard card={card} />;
+  useEffect(() => {
+    fetchCollection()
+      .then(setCollection)
+      .catch(console.error);
+  }, []);
+
+  if (!collection) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (!collection.length) {
+    return <h3>Empty collection</h3>;
+  }
+
+  return <RenderCard card={collection[0]} />;
 };
